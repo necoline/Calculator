3dcf1fe2554aae78e65cb4904428f687a4a7f67f
necoline <hnhubner@gmail.com>
2016-07-27 08:33:46 -0600

    making all the variables one if statement

--- js/main.js
+++ js/main.js
@@ -2,28 +2,27 @@ var sln = document.getElementById("solution");
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
 
-function addOp() {
-  var i1Pi2 = parseFloat(input1.value) + parseFloat(input2.value);
-sln.innerHTML = i1Pi2;
-}
-
-function subOp() {
-  var i1Mi2 = parseFloat(input1.value) - parseFloat(input2.value);
-sln.innerHTML = i1Mi2;
-}
-
-function multOp() {
-  var i1Ti2 = parseFloat(input1.value) * parseFloat(input2.value);
-sln.innerHTML = i1Ti2;
-}
-
-function divOp() {
-  var i1Di2 = parseFloat(input1.value) / parseFloat(input2.value);
-  if (parseFloat(input2) === 0) {
-    alert("You cannot divide by zero");
+function op(sign) {
+  if (sign === '+') {
+    var i1Pi2 = parseFloat(input1.value) + parseFloat(input2.value);
+  sln.innerHTML = i1Pi2;
   }
-  else{
-    sln.innerHTML = i1Di2;
+  else if (sign === '-') {
+    var i1Mi2 = parseFloat(input1.value) - parseFloat(input2.value);
+  sln.innerHTML = i1Mi2;
+  }
+  else if (sign === '*') {
+    var i1Ti2 = parseFloat(input1.value) * parseFloat(input2.value);
+  sln.innerHTML = i1Ti2;
+  }
+  else if (sign === '/') {
+    var i1Di2 = parseFloat(input1.value) / parseFloat(input2.value);
+    if (parseFloat(input2.value) === 0) {
+      alert("You cannot divide by zero");
+    }
+    else{
+      sln.innerHTML = i1Di2;
+    }
   }
 }
 
