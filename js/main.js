bb0171a4f193bdd44bc46fbd116efd956217679a
necoline <hnhubner@gmail.com>
2016-07-27 07:49:57 -0600

    simplifying variables

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
-
+var sln = document.getElementById("solution");
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
 
 function addOp() {
-  var sln = document.getElementById("solution");
-  var input1 = document.getElementById("input1").value;
-  var input2 = document.getElementById("input2").value;
-  var i1Pi2 = parseFloat(input1) + parseFloat(input2);
+  var i1Pi2 = parseFloat(input1.value) + parseFloat(input2.value);
 sln.innerHTML = i1Pi2;
 }
 
 function subOp() {
-  var sln = document.getElementById("solution");
-  var input1 = document.getElementById("input1").value;
-  var input2 = document.getElementById("input2").value;
-  var i1Mi2 = parseFloat(input1) - parseFloat(input2);
+  var i1Mi2 = parseFloat(input1.value) - parseFloat(input2.value);
 sln.innerHTML = i1Mi2;
 }
 
 function multOp() {
-  var sln = document.getElementById("solution");
-  var input1 = document.getElementById("input1").value;
-  var input2 = document.getElementById("input2").value;
-  var i1Ti2 = parseFloat(input1) * parseFloat(input2);
+  var i1Ti2 = parseFloat(input1.value) * parseFloat(input2.value);
 sln.innerHTML = i1Ti2;
 }
 
 function divOp() {
-  var sln = document.getElementById("solution");
-  var input1 = document.getElementById("input1").value;
-  var input2 = document.getElementById("input2").value;
-  var i1Di2 = parseFloat(input1) / parseFloat(input2);
+  var i1Di2 = parseFloat(input1.value) / parseFloat(input2.value);
   if (parseFloat(input2) === 0) {
     alert("You cannot divide by zero");
   }
